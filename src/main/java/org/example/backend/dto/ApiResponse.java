d1b8fee6578e5c49b957f82677afe565db7a2854
NTDung1707 <nguyentrungdung1707@gmail.com>
2025-03-04 14:46:31 +0700

    fix exception'

---
+++ src/main/java/org/example/backend/dto/ApiResponse.java
@@ -0,0 +1,42 @@
+package org.example.backend.dto;
+
+
+public class ApiResponse<T> {
+    private int code ;
+    private String message;
+    private T result;
+
+    public static <T> ApiResponse<T> success(T result) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setCode(200);
+        response.setMessage("Success");
+        response.setResult(result);
+        return response;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+
+}
